3ccd0d99e4cde38bdbeb4fdf4857b446e356333a
closedsum <closedsum@gmail.com>
2018-09-17 13:44:52 -0700

    WIP Weapons

---
+++ Assets/Scripts/Gg/Data/MGgData_Weapon.cs
@@ -0,0 +1,23 @@
+﻿namespace Gg
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    using UnityEngine;
+
+    using CgCore;
+
+    public class MGgData_Weapon : MCgData_ProjectileWeapon
+    {
+        #region "Stats"
+
+        [SerializeField]
+        public S_FGgData_Weapon_FireMode S_FireMode;
+        [NonSerialized]
+        public FGgData_Weapon_FireMode FireMode;
+
+        #endregion // Stats
+
+    }
+}
---
+++ Assets/Scripts/Gg/Types/GgTypes_Weapon.cs
@@ -0,0 +1,46 @@
+﻿namespace Gg
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    using CgCore;
+
+    [Serializable]
+    public struct S_FGgData_Weapon_FireMode
+    {
+        [SerializeField]
+        public S_FCgData_Weapon_FireMode_Firing Firing;
+        [SerializeField]
+        public S_FCgData_Weapon_FireMode_Animation Animation;
+        [SerializeField]
+        public S_FCgData_Weapon_FireMode_Aiming Aiming;
+        [SerializeField]
+        public S_FCgData_Weapon_FireMode_FXs FXs;
+        [SerializeField]
+        public S_FCgData_Weapon_FireMode_Sounds Sounds;
+    }
+
+    public class FGgData_Weapon_FireMode
+    {
+        public FCgData_Weapon_FireMode_Firing Firing;
+
+        public FCgData_Weapon_FireMode_Animation Animation;
+
+        public FCgData_Weapon_FireMode_Aiming Aiming;
+
+        public FCgData_Weapon_FireMode_FXs FXs;
+
+        public FCgData_Weapon_FireMode_Sounds Sounds;
+
+        public FGgData_Weapon_FireMode()
+        {
+            Firing = new FCgData_Weapon_FireMode_Firing();
+            Animation = new FCgData_Weapon_FireMode_Animation();
+            Aiming = new FCgData_Weapon_FireMode_Aiming();
+            FXs = new FCgData_Weapon_FireMode_FXs();
+            Sounds = new FCgData_Weapon_FireMode_Sounds();
+        }
+    }
+}
